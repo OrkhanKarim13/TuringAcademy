349059e57be6bfdebb5f54b8e769f17d63260f53
OrkhanKarim13 <mrorkhankarimli@gmail.com>
2024-03-18 01:12:54 +0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+document.getElementById("toggleDownMenu").addEventListener("click", function() {
+    var downMenu = document.querySelector(".downMenu");
+    downMenu.style.display = downMenu.style.display === "none" ? "block" : "none";
+    downMenu.style.top = downMenu.style.top === "100px" ? "150px" : "100px"; // Eğer downMenu görünürse, 100px aşağıda değilse 150px aşağıda göster
+  });
