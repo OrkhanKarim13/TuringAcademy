97b9820876c10d5f2c61342104465f46964b6f98
OrkhanKarim13 <mrorkhankarimli@gmail.com>
2024-03-18 19:58:48 +0400

    Codes Fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
-document.getElementById("toggleDownMenu").addEventListener("click", function() {
+document
+  .getElementById("toggleDownMenu")
+  .addEventListener("click", function () {
     var downMenu = document.querySelector(".downMenu");
-    downMenu.style.display = downMenu.style.display === "none" ? "block" : "none";
+    downMenu.style.display =
+      downMenu.style.display === "none" ? "block" : "none";
     downMenu.style.top = downMenu.style.top === "100px" ? "150px" : "100px"; // Eğer downMenu görünürse, 100px aşağıda değilse 150px aşağıda göster
   });
